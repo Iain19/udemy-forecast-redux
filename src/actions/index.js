fd45611782fc6170f946144293fb69f1eaec6d56
Igor Khudik <js.web23@gmail.com>
2016-06-13 23:17:50 +0300

    Add middleware and ajax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
-const API_KEY = '2de143494c0b295cca9337e1e96b00e0';
+import axios from 'axios';
+
+const API_KEY = '27b9ef192d18684e14dcc4f542a53c77';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    console.log('Request:', request);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+};
