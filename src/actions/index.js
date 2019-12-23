9a689adf3fc52cd9908ac068cbd2afee0cc262ec
ihork <js.web23@gmail.com>
2019-12-23 11:37:55 +0200

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '27b9ef192d18684e14dcc4f542a53c77';
+const API_KEY = '_';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
